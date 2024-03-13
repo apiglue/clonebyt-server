5e9df609f1f6bf9f2be965e041afce7c33321919
marcelo <marcelo79@gmail.com>
2024-03-13 11:27:55 -0500

    reworking test structure

---
+++ src/test/java/io/apiglue/clonebyt/clonebytserver/ApplicationTests.java
@@ -0,0 +1,15 @@
+package io.apiglue.clonebyt.clonebytserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@ContextConfiguration(classes = Application.class)
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
