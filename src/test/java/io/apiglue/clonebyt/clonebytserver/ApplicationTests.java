787fd3bd8f6c279dfc74f2d277825f22c4fd9fad
marcelo <marcelo79@gmail.com>
2024-03-15 14:34:20 -0500

    now loading ApplicationTests properties

--- src/test/java/io/apiglue/clonebyt/clonebytserver/ApplicationTests.java
+++ src/test/java/io/apiglue/clonebyt/clonebytserver/ApplicationTests.java
@@ -2,9 +2,10 @@ package io.apiglue.clonebyt.clonebytserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(classes = Application.class)
-
+@ActiveProfiles("test")
 public class ApplicationTests {
 
     @Test
