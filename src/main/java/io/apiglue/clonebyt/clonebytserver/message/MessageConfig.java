ff48f850a3dddda7509b19a810e0a8b98e9fcbe1
marcelo <marcelo79@gmail.com>
2024-03-15 15:21:55 -0500

    removing unnecessary app properties

--- src/main/java/io/apiglue/clonebyt/clonebytserver/message/MessageConfig.java
+++ src/main/java/io/apiglue/clonebyt/clonebytserver/message/MessageConfig.java
@@ -4,15 +4,13 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.time.LocalDateTime;
-
 @Configuration
 public class MessageConfig {
     @Bean
     CommandLineRunner commandLineRunner(MessageRepository messageRepository) {
         return args -> {
-            Message message = new Message("Hello, World", LocalDateTime.now().plusYears(1));
-            messageRepository.save(message);
+//            Message message = new Message("Hello, World", LocalDateTime.now().plusYears(1));
+//            messageRepository.save(message);
         };
     }
 }
